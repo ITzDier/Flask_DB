f12e3b9d6e0517868fbfce965f41e541c40db88d
Jesus B <itzdier862@gmail.com>
2025-08-31 18:02:25 -0600

    Se implementaron los archivos correspondientes para el uso de la aplicación

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:123@localhost:5432/Flask_DB'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Aquí puedes añadir más configuraciones
